e5fbf4806b20861b81b69002cd2b1f175e62adb8
g1alexander <alexlds26@gmail.com>
2021-11-13 16:31:25 -0500

    feat: update config PWA

---
+++ components/Head.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+
+export default function HeadComponent({ frontmatter }) {
+  return (
+    <Head>
+      <title>
+        Alexander Granados{frontmatter && ` | ${frontmatter.title}`}
+      </title>
+      <meta
+        name="description"
+        content="Portfolio and blog website by g1alexander"
+      />
+
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="manifest" href="/manifest.json" />
+      <meta name="theme-color" content="#DBF0EB" />
+      <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png"></link>
+    </Head>
+  );
+}
