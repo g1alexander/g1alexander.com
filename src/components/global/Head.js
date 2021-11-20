e30034043098bef5d4718b5d348c8f48b396f115
g1alexander <alexlds26@gmail.com>
2021-11-20 16:08:16 -0500

    feat: metadata seo

--- src/components/global/Head.js
+++ src/components/global/Head.js
@@ -16,39 +16,48 @@ export function HeadComponent({ frontmatter }) {
       <meta name="theme-color" content="#DBF0EB" />
       <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png"></link>
 
-      <meta name="twitter:card" content="summary" />
-      <meta name="twitter:site" content="@g1alexander" />
-      <meta name="twitter:creator" content="@g1alexander" />
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta author="Alexander Granados" />
 
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:creator" content="@g1alexander" />
+      <meta name="twitter:domain" content="g1alexander.com" />
+      <meta name="twitter:site" content="@g1alexander" />
       <meta
-        property="og:url"
-        content="https://devg1alexander-com.vercel.app/"
-        key="ogurl"
+        name="twitter:title"
+        content="Alexander Granados 👨🏼&zwj;💻 | Desarrollador Web"
       />
       <meta
-        property="og:image"
-        content="http://graphics8.nytimes.com/images/2011/12/08/technology/bits-newtwitter/bits-newtwitter-tmagArticle.jpg"
-        key="ogimage"
+        name="twitter:description"
+        content="Portfolio and blog website by g1alexander | Alexander Granados"
       />
-      <meta
-        property="og:site_name"
-        content={`Alexander Granados${
-          frontmatter && ` | ${frontmatter.title}`
-        }`}
-        key="ogsitename"
-      />
+
+      <meta property="og:url" content="https://g1alexander.com" />
+      <meta property="og:type" content="website" />
+      <meta property="og:locale" content="es_ES" />
       <meta
         property="og:title"
-        content={`Alexander Granados${
-          frontmatter && ` | ${frontmatter.title}`
-        }`}
-        key="ogtitle"
+        content="Alexander Granados 👨🏼&zwj;💻 | Desarrollador Web"
       />
       <meta
         property="og:description"
-        content="Portfolio and blog website by g1alexander."
-        key="ogdesc"
+        content="Portfolio and blog website by g1alexander | Alexander Granados"
       />
+      <meta property="og:site_name" content="g1alexander.com" />
+      <meta
+        property="og:image"
+        content="https://res.cloudinary.com/dlgvxohur/image/upload/v1627483439/proyectos/g1alexander.webp"
+      />
+      <meta
+        property="og:image"
+        content="https://res.cloudinary.com/dlgvxohur/image/upload/v1627483439/proyectos/g1alexander.webp"
+      />
+      <meta
+        property="og:image:alt"
+        content="Alexander Granados 👨🏼&zwj;💻 | Desarrollador Web"
+      />
+      <meta property="og:image:type" content="image/jpeg" />
     </Head>
   );
 }
