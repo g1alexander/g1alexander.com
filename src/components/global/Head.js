d6167f2ea341632f12d794b275ffb6bfe5c25fbe
g1alexander <alexlds26@gmail.com>
2021-11-20 15:33:35 -0500

    feat: add keys meta

--- src/components/global/Head.js
+++ src/components/global/Head.js
@@ -19,23 +19,35 @@ export function HeadComponent({ frontmatter }) {
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:site" content="@g1alexander" />
       <meta name="twitter:creator" content="@g1alexander" />
+
       <meta
         property="og:url"
-        content="http://bits.blogs.nytimes.com/2011/12/08/a-twitter-for-my-sister/"
+        content="https://devg1alexander-com.vercel.app/"
+        key="ogurl"
+      />
+      <meta
+        property="og:image"
+        content="http://graphics8.nytimes.com/images/2011/12/08/technology/bits-newtwitter/bits-newtwitter-tmagArticle.jpg"
+        key="ogimage"
+      />
+      <meta
+        property="og:site_name"
+        content={`Alexander Granados${
+          frontmatter && ` | ${frontmatter.title}`
+        }`}
+        key="ogsitename"
       />
       <meta
         property="og:title"
         content={`Alexander Granados${
           frontmatter && ` | ${frontmatter.title}`
         }`}
+        key="ogtitle"
       />
       <meta
         property="og:description"
         content="Portfolio and blog website by g1alexander."
-      />
-      <meta
-        property="og:image"
-        content="http://graphics8.nytimes.com/images/2011/12/08/technology/bits-newtwitter/bits-newtwitter-tmagArticle.jpg"
+        key="ogdesc"
       />
     </Head>
   );
