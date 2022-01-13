5d9909b1033a20554b743d98301f57bc2fdcecd7
g1alexander <alexlds26@gmail.com>
2022-01-12 21:47:30 -0500

    feat: update of component footer

--- src/components/global/Footer.jsx
+++ src/components/global/Footer.jsx
@@ -1,24 +1,40 @@
 import { getSocialMedia } from "@/utils/getSocialMedia";
-import Icon from "@mdi/react";
+import Image from "next/image";
+import Logo from "@/static/logo.svg";
 
 export function Footer() {
   const socialMedia = getSocialMedia();
+
   return (
-    <footer className="flex flex-col justify-center items-center py-8">
-      <section className="flex mb-3">
-        {socialMedia.map(({ id, link, icon }) => (
-          <a key={id} href={link} target="_blank" rel="noreferrer">
-            <Icon
-              path={icon}
-              size={1.4}
-              className="text-green-800 dark:text-green-300 mx-2"
-            />
-          </a>
-        ))}
+    <footer className="bg-white-400">
+      <section className=" flex justify-between items-center container mx-auto px-5 py-10">
+        <div>
+          <h3 className="font-semibold my-3">Redes</h3>
+          <ul className="flex flex-col">
+            {socialMedia.map(({ id, link, name }) => (
+              <a
+                key={id}
+                href={link}
+                target="_blank"
+                rel="noreferrer"
+                className="hover:text-blue-500 my-1"
+              >
+                {name}
+              </a>
+            ))}
+          </ul>
+        </div>
+        <div className="flex items-center">
+          <div className="text-xl mr-3">
+            <h3 className="my-2">Alexander Granados</h3>
+            <hr className="w-3/4 mx-auto" />
+          </div>
+          <Image alt="g1alexander" src={Logo} width={40} height={40} />
+        </div>
       </section>
-      <span className="text-green-800 dark:text-green-300">
+      <div className="text-white-700 bg-white-500 text-center py-3">
         Copyright &copy; {new Date().getFullYear()} All rights reserved.
-      </span>
+      </div>
     </footer>
   );
 }
--- src/utils/getSocialMedia.js
+++ src/utils/getSocialMedia.js
@@ -7,24 +7,28 @@ export function getSocialMedia() {
       link: "https://linkedin.com/in/g1alexander/",
       icon: mdiLinkedin,
       color: 800,
+      name: "LinkedIn",
     },
     {
       id: 2,
       link: "https://github.com/g1alexander",
       icon: mdiGithub,
       color: 800,
+      name: "Github",
     },
     {
       id: 3,
       link: "https://twitter.com/g1alexander_",
       icon: mdiTwitter,
       color: 600,
+      name: "Twitter",
     },
     {
       id: 4,
       link: "https://instagram.com/g1alexander/",
       icon: mdiInstagram,
       color: 600,
+      name: "Instagram",
     },
   ];
 }
