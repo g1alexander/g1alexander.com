1f68d4f8303077cca1af75d35ed4f2c43415dbe7
g1alexander <alexlds26@gmail.com>
2022-01-15 17:14:08 -0500

    feat: component skills

---
+++ src/components/global/Skills.jsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+export function Skills({ skills }) {
+  return skills.map(({ id, name, logo, logoColor, color }) => (
+    <span key={id} className="mr-2">
+      <Image
+        alt={name}
+        src={`https://img.shields.io/badge/-${name}-${color}?&logo=${logo}&logoColor=${logoColor}`}
+        width={70}
+        height={25}
+        className=""
+      />
+    </span>
+  ));
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      skills,
+    },
+  };
+}
