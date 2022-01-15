e10cd6a9a1670b356f8eaf1512367c8a1a411c19
g1alexander <alexlds26@gmail.com>
2022-01-15 12:16:44 -0500

    feat: page home

--- src/components/projects/ProjectsPreview.jsx
+++ src/components/projects/ProjectsPreview.jsx
@@ -1,20 +1,44 @@
 import { useMatchMedia } from "@/hooks/useMatchMedia";
 import Image from "next/image";
+import Link from "next/link";
 
 export function ProjectsPreview() {
   const matchMedia = useMatchMedia();
 
   return (
     <>
-      <section className="md:flex w-full my-7">
-        <Image
-          src="https://res.cloudinary.com/dlgvxohur/image/upload/v1627482218/proyectos/CatWiki/portada.webp"
-          alt="Picture of the author"
-          width={290}
-          height={210}
-          layout={!matchMedia && "responsive"}
-          className="rounded w-1/5"
-        />
+      <h3 className="font-semibold">Proyectos ‍💻</h3>
+      <section className="md:flex w-full mt-7 mb-20 grid-me">
+        <Link href={`/blog/`} passHref>
+          <Image
+            src="https://res.cloudinary.com/dlgvxohur/image/upload/v1627482218/proyectos/CatWiki/portada.webp"
+            alt="Picture of the author"
+            width={290}
+            height={210}
+            layout={!matchMedia && "responsive"}
+            className="rounded w-1/5"
+          />
+        </Link>
+        <Link href={`/blog/`} passHref>
+          <Image
+            src="https://res.cloudinary.com/dlgvxohur/image/upload/v1627482218/proyectos/CatWiki/portada.webp"
+            alt="Picture of the author"
+            width={290}
+            height={210}
+            layout={!matchMedia && "responsive"}
+            className="rounded w-1/5"
+          />
+        </Link>
+        <Link href={`/blog/`} passHref>
+          <Image
+            src="https://res.cloudinary.com/dlgvxohur/image/upload/v1627482218/proyectos/CatWiki/portada.webp"
+            alt="Picture of the author"
+            width={290}
+            height={210}
+            layout={!matchMedia && "responsive"}
+            className="rounded w-1/5"
+          />
+        </Link>
       </section>
     </>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,26 @@
 import { getAllFilesMetadata } from "@/lib/mdx";
 import { HeadComponent } from "@/components/global/Head";
-// import { ProjectsPreview } from "@/components/projects/ProjectsPreview";
+import { ProjectsPreview } from "@/components/projects/ProjectsPreview";
 
 export default function Home({ blog, projects }) {
+  let vue = "vue";
+  console.log(vue);
   return (
     <>
       <HeadComponent />
 
       <main>
         <div className="mx-auto xs:w-11/12 mds:w-9/12">
-          {/* <ProjectsPreview /> */}
+          <div className="w-3/4 mx-auto py-32">
+            <h1 className="text-4xl  text-center">
+              Hola👋🏼 Soy Alexander Granados, desarrollador Front-end en{" "}
+              <span className="font-light written">
+                <span className="text-tech-vue font-semibold">Vue</span>/
+                <span className="text-tech-react font-semibold">React</span>
+              </span>
+            </h1>
+          </div>
+          <ProjectsPreview />
         </div>
       </main>
     </>
