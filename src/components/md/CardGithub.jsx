35b78613ca42df977ca01853fad2fe1cd25a5d6d
g1alexander <alexlds26@gmail.com>
2022-01-18 22:35:02 -0500

    fix: script of cardgithub

--- src/components/md/CardGithub.jsx
+++ src/components/md/CardGithub.jsx
@@ -1,7 +1,16 @@
+import Script from "next/script";
+
 export function CardGithub({ data }) {
   const { username, repo } = data;
 
   return (
-    <div className="repo-card my-5" data-repo={`${username}/${repo}`}></div>
+    <>
+      <Script
+        src="https://g1alexander.github.io/repo-card/repo-card.js"
+        strategy="beforeInteractive"
+      />
+
+      <div className="repo-card my-5" data-repo={`${username}/${repo}`}></div>
+    </>
   );
 }
