6dd1c1666122222dcd6e2784ab622cbeaa5ad047
g1alexander <alexlds26@gmail.com>
2022-01-19 22:03:27 -0500

    fix: delete component of github

--- src/components/md/MDXComponent.js
+++ src/components/md/MDXComponent.js
@@ -1,11 +1,9 @@
 import { ImageMe } from "./ImageMe";
 import { SpanMe } from "./SpanMe";
-import { CardGithub } from "./CardGithub";
 
 const MDXComponent = {
   ImageMe,
   SpanMe,
-  CardGithub,
   h1: (props) => <h1 className="font-bold text-3xl my-7" {...props}></h1>,
   h3: (props) => <h3 className="font-bold text-lg mt-5" {...props}></h3>,
   p: (props) => <p className="text-md my-3" {...props}></p>,
