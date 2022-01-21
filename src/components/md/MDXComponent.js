442b936ad49f583f877db8966f8573a168a607ad
g1alexander <alexlds26@gmail.com>
2022-01-20 21:35:55 -0500

    feat: project rentme

--- src/components/md/MDXComponent.js
+++ src/components/md/MDXComponent.js
@@ -1,9 +1,13 @@
 import { ImageMe } from "./ImageMe";
 import { SpanMe } from "./SpanMe";
+import { Iframe } from "./Iframe";
+import { Skills } from "@/components/global/Skills";
 
 const MDXComponent = {
+  Skills,
   ImageMe,
   SpanMe,
+  Iframe,
   h1: (props) => <h1 className="font-bold text-3xl my-7" {...props}></h1>,
   h3: (props) => <h3 className="font-bold text-lg mt-5" {...props}></h3>,
   p: (props) => <p className="text-md my-3" {...props}></p>,
